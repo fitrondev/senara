fca790d29218d4abf91a04a32d14641e53593d36
fitrondev <muhfitron7@gmail.com>
2026-05-20 08:51:30 +0800

    fix: correct logo file name casing in Logo component

--- apps/web/src/components/root/Header/Logo.tsx
+++ apps/web/src/components/root/Header/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <>
-      <Image src="/logo/senara.svg" alt="Logo" width={32} height={32} />
+      <Image src="/logo/Senara.svg" alt="Logo" width={32} height={32} />
     </>
   );
 };
